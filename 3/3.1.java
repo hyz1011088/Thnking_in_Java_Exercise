1bee20dd617789ae2bcc39befb1914d95b6df410
hyz1011088 <hyz1011088@gmail.com>
2015-09-09 22:55:58 +0800

    exe2-exe3

---
+++ 3/3.1.java
@@ -0,0 +1,12 @@
+package main;
+
+import static net.mindview.util.Print.*;
+import org.greggordon.tools.*;
+
+public class PrintTest {
+	public static void main(String[] args) {
+		print("Hello, from short form.");
+		P.rintln("Hello from greggordon form.");
+		System.out.println("Hello from long form.");
+	}
+}
